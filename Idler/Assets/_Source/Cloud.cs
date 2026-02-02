3dfac98401f1ae4d7cc10e20218061390734cbf8
ChalkMel <melnikvv24@st.ithub.ru>
2026-02-02 18:10:21 +0300

    Main UI Prototypes
    Button to tea UI, button to frog UI, prototype of shop

--- Idler/Assets/_Source/Cloud.cs
+++ Idler/Assets/_Source/Cloud.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
-
+using DG.Tweening;
 public class Cloud : MonoBehaviour
 {
     [SerializeField] private Credits credits;
-
+    [SerializeField] private Vector2 punchScale;
+    [SerializeField] private float duration;
     private void OnMouseDown()
     {
         credits.DropletsDrop();
+        transform.DOPunchScale(punchScale, duration);
     }
 }
--- Idler/Assets/_Source/MainButtons.cs
+++ Idler/Assets/_Source/MainButtons.cs
@@ -1,16 +1,62 @@
+using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using DG.Tweening;
 public class MainButtons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Buttons")]
+    [SerializeField] private Button shopButton;
+    [SerializeField] private Button frogButton;
+    [SerializeField] private Button deerButton;
+    [SerializeField] private float duration;
+    [SerializeField] private List<Button> closeButton;
+    [Header("Frog")]
+    [SerializeField] private CanvasGroup frogUI;
+    [Header("Deer")]
+    [SerializeField] private CanvasGroup deerUI;
+    [Header("Shop")]
+    [SerializeField] private CanvasGroup shopUI;
+    
+    private Tween _fadeTween;
+    private void Start()
     {
+        shopButton.onClick.AddListener(OpenShopUI);
+        frogButton.onClick.AddListener(OpenFrogUI);
+        deerButton.onClick.AddListener(OpenDeerUI);
+        foreach (Button t in closeButton)
+        {
+            t.onClick.AddListener(CloseUI);
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void OpenFrogUI()
     {
-        
+        Open(frogUI);
+    }
+
+    private void Open(CanvasGroup canvasGroup)
+    {
+        CloseUI();
+        canvasGroup.gameObject.SetActive(true);
+        canvasGroup.alpha = 0;
+        _fadeTween = canvasGroup.DOFade(1f, duration);
+    }
+    private void OpenDeerUI()
+    {
+        Open(deerUI);
+    }
+    
+    private void OpenShopUI()
+    {
+        Open(shopUI);
+    }
+
+    private void CloseUI()
+    {
+        _fadeTween?.Kill();
+        deerUI.gameObject.SetActive(false);
+        frogUI.gameObject.SetActive(false);
+        shopUI.gameObject.SetActive(false);
     }
 }
